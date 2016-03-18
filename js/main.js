a50de16ea339d5d4a449726d200893c3a821e786
Nasrine <azar0006@algonquinlive.com>
2016-03-18 10:46:24 -0400

    changed the #-id to .-class

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 var $input = $('#ball');
-var
 
-$('#btn-right').on('click', function (){
-  var newLeft = $ball.offset().left + 30;
-  $ball.css('left', newLeft);
+$('form').on('change', function (){
+  $ball.css('background colour', );
 });
-
